d69bc13aee79c628ba0acdd891b37721de576990
Filip Trajkoski <89231028@student.upr.si>
2025-06-06 12:09:44 +0200

    Made a PieceScript, which will be added to the Piece markers (since their position changes in the physical world, the child of the Piece marker, i.e. the actual GameObject piece will always have position 0,0,0 as a child of the image marker)

--- Assets/Scripts/PieceScript.cs
+++ Assets/Scripts/PieceScript.cs
@@ -1,11 +1,91 @@
+using System.Collections;
 using UnityEngine;
 
 public class PieceScript : MonoBehaviour
 {
+
+    // board position variables
+    private int oldX, oldY;
+    private int newX, newY;
+    // end board position variables
+
+
+    // physical position variables
+    private Vector3 last_position;
+    // end physical position variables
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // setting default values
+        oldX = oldY = newX = newY = -1;
+        last_position = transform.position;
+
+        StartCoroutine("OnMoveCoroutine");
+    }
+
+    IEnumerator OnMoveCoroutine() 
+    {
+        while (true) {
+            yield return new WaitUntil(Moved);
+            
+            DetectSquare(); // raycast down to see which tile/spot you're on
+
+            // if newX, newY are -1, i.e. no tile is under the piece, so continue
+            if (newX == -1 || newY == -1) continue;
+
+            // else
+            // compare oldX, oldY with newX, newY
+            if (oldX != newX || oldY != newY) { 
+                // TODO
+                // - send signal to GameManagement
+
+                // if move is ok, GameManagement will call ConfirmMove()
+            }
+
+        }
+    }
+
+    private bool Moved()
+    {
+        Vector3 new_position = transform.position;
+        if (last_position != new_position) {
+            last_position = new_position;
+            return true;
+        }
+        return false;
+    }
+
+
+    private void DetectSquare() {
+
+        // 1) Raycast down and see if it's a tile under the piece
+        Ray ray = new Ray(transform.position, Vector3.down);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit) && hit.transform.CompareTag("board_tile"))
+        {
+            // get its x, y
+            // set newX, newY
+            SpotScript spotScript = hit.collider.GetComponent<SpotScript>();
+            newX = spotScript.GetX();
+            newY = spotScript.GetY();
+        }
+        else 
+        {
+            // no tile under
+            newX = newY = -1;
+        }
+
+
+
+    }
+
+    public void ConfirmMove(int x, int y) {
+        // passing parameters, in case piece is moved while move is being confirmed
+        oldX = newX = x;
+        oldY = newY = y;
     }
 
     // Update is called once per frame
